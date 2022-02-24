c7e6cc31ec3cd57a0d08124e0dda01dff2126d41
Dominik Fedyk <49025848+Dlyam@users.noreply.github.com>
2022-02-24 21:12:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const text = document.querySelector('#text_section');
+const contact = document.querySelector('');
+
+function MouseCheckOnHTML(){
+    text.innerText = 'HTML5 - I learned this very quickly, I can create a simple template, make a list, table or meta additions.';
+}
+
+function MouseCheckOnCSS(){
+    text.innerText = 'CSS - I can decorate a page with a stylesheet, I can use RWD (Responsive Web Design), import various fonts.';
+}
+
+function MouseCheckOnJS(){
+    text.innerText = 'Javascript - I can write functions, events and manipulate the DOM object, use various libraries.';
+}
+
+function MouseCheckOnJQ(){
+    text.innerText = 'jQuery - I can use this javascript library best, I can create effective transitions through DOM objects.';
+}
+
+function MouseCheckOnKrita(){
+    text.innerText = 'Krita - I can use a wide range of tools to create an artistic work, I use a graphic tablet.';
+}
+
+function MouseOut(){
+    text.innerText = 'Programmer (Front-End) & Artist';
+}
+
